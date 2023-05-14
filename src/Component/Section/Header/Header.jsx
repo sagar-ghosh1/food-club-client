a2756a31d7aeb7f6cc7698bb09a71eb36eaf58a3
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-14 14:18:32 +0600

    header par

--- src/Component/Section/Header/Header.jsx
+++ src/Component/Section/Header/Header.jsx
@@ -4,15 +4,15 @@ import LazyLoad from 'react-lazy-load';
 const Header = () => {
     return (
         <div>
-            <div className="hero py-10 bg-pink-100">
+            <div className="hero py-10 bg-green-50">
                 <div className="hero-content flex-col lg:flex-row">
                     <LazyLoad>
-                        <img src="https://rb.gy/yi140" className="lg:max-w-xl lg:mr-12 rounded-lg shadow-2xl" />
+                        <img src="https://rb.gy/ucvnt" className="lg:max-w-xl lg:mr-12 rounded-lg shadow-2xl" />
                     </LazyLoad>
                     <div>
-                        <h1 className="text-5xl font-bold leading-[70px]">Curly Cheese & Vegetable Sandwich</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi. Leo urna molestie at elementum eu facilisis sed odio. Malesuada fames ac turpis egestas integer. Porttitor massa id neque aliquam vestibulum morbi blandit nisl cursus risus.</p>
-                        <button className="btn btn-outline">Order Now</button>
+                        <h1 className="text-5xl font-bold leading-[70px]">A Journey of Coffee, Cuisine, and Community</h1>
+                        <p className="py-6">Welcome to A Journey of Coffee, Cuisine, and Community! Step into a world where the aromatic allure of coffee blends seamlessly with delectable cuisine and a warm sense of community. Our establishment is more than just a café; it is a gathering place where friendships are forged, conversations flow, and unforgettable experiences are created.</p>
+                        <button className="btn btn-outline btn-success">Order Now</button>
                     </div>
                 </div>
             </div>
