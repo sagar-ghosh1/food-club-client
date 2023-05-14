4a17fc80d8ef0fc26cfc23f27ea0c602e081d8c4
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-14 15:48:55 +0600

    about section

--- src/Component/pages/About/About.jsx
+++ src/Component/pages/About/About.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className="hero min-h-screen bg-fixed bg-[url('https://rb.gy/oe2bq')]">
-            <h2 className='text-5xl text-slate-200 font-semibold text-center'>coming soon...</h2>
+        <div className="hero min-h-screen bg-fixed bg-[url('https://rb.gy/h1o8e')]">
+            <h2 className='text-5xl text-purple-600 font-semibold text-center'>coming soon...</h2>
         </div>
     );
 };
