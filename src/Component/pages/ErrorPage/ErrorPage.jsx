9c1526659afc4b2361af5ff7ded8db5d922b4bbc
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-14 21:24:23 +0600

    error page

--- src/Component/pages/ErrorPage/ErrorPage.jsx
+++ src/Component/pages/ErrorPage/ErrorPage.jsx
@@ -7,15 +7,15 @@ import LazyLoad from 'react-lazy-load';
 const ErrorPage = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-indigo-200">
+            <div className="hero min-h-screen bg-slate-300">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <LazyLoad>
                         <img src={notFound} className="lg:max-w-lg rounded-lg shadow-2xl" />
                     </LazyLoad>
 
                     <div className='mr-10'>
-                        <h1 className="text-5xl font-bold">Oops, page not found</h1>
-                        <p className="py-6">Sorry, the page you are looking for could not be found</p>
+                        <h1 className="text-5xl font-bold">Oops! <br /> Page not found</h1>
+                        <p className="py-6">The page you are looking for could not be found</p>
                         <Link className="btn btn-outline btn-primary"><FaArrowLeft className='mr-2' /> Back To Home</Link>
                     </div>
                 </div>
